4212ae65fe356b56f23844be07fa66f292686bce
GN <gn@GNs-MacBook-Pro.local>
2025-03-17 13:46:46 +0530

    a trivial code for generating random ints

---
+++ some_randomness.py
@@ -0,0 +1,14 @@
+import random as random
+
+min_val = 0
+max_val = 10
+
+num_gens = 10
+
+random_int_list = []
+
+for i in range(num_gens):
+    random_int_list.append(random.randint(min_val, max_val))
+
+print(f"your random list of ints between {min_val} and {max_val} is {random_int_list}")
+
